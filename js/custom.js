d63de6d2a5ee3f9bb1fe035ee6260114cf965d6f
unknown <you@example.com>
2021-05-08 18:06:31 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(function(){
+    // banner slider
+    $('.slider-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows:true,
+        fade:true,
+        speed:1500,
+        prevArrow:'.b-left',
+        nextArrow:'.b-right',
+        autoplaySpeed: 2000,
+      });
+    
+    
+    
+    
+    
+    });
